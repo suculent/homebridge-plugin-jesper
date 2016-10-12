6398a070691bc17bfa9a1ddeeba6e8da15433b02
Matěj Sychra <igraczech@igraczech.com>
2016-10-12 21:50:46 +0200

    simplier tests, improved getFixtureState callback implementation

--- index.js
+++ index.js
@@ -7,47 +7,49 @@ var request = require("request");
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
-    Service = homebridge.hap.Service;
-    Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-led", "Jesper-LED", JesperAccessory);
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("homebridge-led", "Jesper-LED", JesperAccessory);
 }
 
 function JesperAccessory(log, config) {
-    this.hpi = new HAL();
-    this.log = log;
-    this.config = config;
-    this.name = config["name"];
-    this.address = 4;
-    
-    this.service
-        .getCharacteristic(Characteristic.On)
-        .on('get', this.getOn.bind(this))
-        .on('set', this.setOn.bind(this));
+  this.hpi = new HAL();
+  this.log = log;
+  this.config = config;
+  this.name = config["name"];
+  this.address = 4;
+  
+  this.service
+  .getCharacteristic(Characteristic.On)
+  .on('get', this.getOn.bind(this))
+  .on('set', this.setOn.bind(this));
 }
 
 JesperAccessory.prototype.getOn = function(callback) {    
-  var status = this.hal.getFixtureState(address);
-  if (callback) {
-    callback(null, status);
-  }
+  this.hal.getFixtureState(address, function (status) {
+    console.log("getFixtureState response:"+status.toString());
+    if (callback) {
+      callback(null, status);
+    }
+  });  
 }
 
 JesperAccessory.prototype.setOn = function(on, callback) {
-    var fixtureState = on ? 1 : 0;
-    this.hal.setFixtureState(address, fixtureState);
-    if (callback) {
-      callback(null, on);
-    }
+  var fixtureState = on ? 1 : 0;
+  this.hal.setFixtureState(address, fixtureState);
+  if (callback) {
+    callback(null, on);
+  }
 }
 
 JesperAccessory.prototype.getServices = function() {
-    return [this.service];
+  return [this.service];
 }
 
 /// HANDLERS
 
 process.on('uncaughtException', function (err) {
-    console.log(err);
+  console.log(err);
 }); 
 
 /// TESTS
@@ -57,42 +59,49 @@ if (testing) {
   var hpi = new HAL(); 
 
   setTimeout(function() {
-      console.log('testing: getFixtureState(4)');
-      hpi.getFixtureState(9, function (state) {
-        console.log(state);
-      });
-  }, 0);
+    console.log('testing: getFixtureState(5)');
+    hpi.getFixtureState(4, function (state) {
+      console.log("getFixtureState response:"+state.toString());
+    });
+  }, 10000);
 
   setTimeout(function() {
-      console.log('testing: setFixtureState(4, 1)');
-      hpi.setFixtureState(4, 1);
-  }, 1000);
+    console.log('testing: setFixtureState(on)');
+    hpi.setFixtureState(4, 1);
+  }, 3000);
+
+  setTimeout(function() {
+    console.log('testing: setFixtureState(off)');
+    hpi.setFixtureState(4, 0);
+  }, 6000);
   
+  /*
   setTimeout(function() {
       console.log('testing: setFixtureRGB(1, 0, 0)');
       hpi.setFixtureRGB(1, 0, 0);
-  }, 2000);
+  }, 6000);
 
   setTimeout(function() {
       console.log('testing: setFixtureRGB(0, 1, 0)');
       hpi.setFixtureRGB(0, 1, 0);
-  }, 4000);
+  }, 9000);
 
   setTimeout(function() {
       console.log('testing: setFixtureRGB(0, 0, 1)');
       hpi.setFixtureRGB(0, 0, 1);
-  }, 6000);
+  }, 12000);
 
   setTimeout(function() {
       console.log('testing: setFixtureRGB(0, 0, 0)');
       hpi.setFixtureRGB(0, 0, 0);
-  }, 8000);
+  }, 15000);
 
   setTimeout(function() {
       console.log('testing: getFixtureState(4)');
-      hpi.getFixtureState(9, function (state) {
-        console.log(state);
+      hpi.getFixtureState(5, function (state) {
+        console.log(state.toString());
       });
-  }, 10000);
+  }, 18000);
+  */
 
 }
